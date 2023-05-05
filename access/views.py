a0bc9491c51e844eeb733b5d05e753de148abe8d
Catalina Fajardo <117235705+TCfajardo@users.noreply.github.com>
2023-05-05 13:27:20 -0500

    Pagina base de la app, con urls

--- access/views.py
+++ access/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'access/home.html')
+
+def products(request):
+    return render(request, 'access/products.html')
