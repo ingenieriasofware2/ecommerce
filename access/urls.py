885494028b059e8b83e99c5a299c6f9e82e61dc5
Catalina Fajardo <117235705+TCfajardo@users.noreply.github.com>
2023-05-08 12:38:37 -0500

    Form login witch security

--- access/urls.py
+++ access/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import home, products
+from .views import home, products, exit
 
 urlpatterns = [
     path('', home, name='home'),
     path('products/', products, name='products'),
+    path('logout/', exit, name='exit'),
 ]
--- my_ecommerce/urls.py
+++ my_ecommerce/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('access.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
     
 ]
